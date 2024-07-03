67dbef5aedcd8f5f2a894367500df36285cfb3b8
marzeverett <marzeverett@gmail.com>
2024-07-03 10:04:33 -0700

    Trying something

--- tests/local_mongo_report.py
+++ tests/local_mongo_report.py
@@ -27,6 +27,7 @@ for database in client.database_names():
                 last_n_docs = col.find().skip(col.count() - n)
                 print(f"Last {n} Documents:")
                 for doc in last_n_docs:
+                    print(doc)
                     json.dumps(doc, indent=4, default=str)
             except Exception as e:
                 print(f"Could not report on collection {collection_name}; {e}")
