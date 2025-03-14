8c56057b6da329bac67d234f092b48eaba5e94cd
marzeverett <marzeverett@gmail.com>
2025-03-14 15:38:38 -0700

    Fixed s3 and camera test

--- tests/test_camera_to_s3.py
+++ tests/test_camera_to_s3.py
@@ -1,19 +1,9 @@
-import sys
-import time 
-sys.path.append("../scarecro")
-import logging 
-import system_object
-#Get the system config
-import src.system.system as system_class 
-logging.basicConfig(level=logging.DEBUG, format='%(levelname)s - %(asctime)s - %(message)s')
-
-
+import configuration_tester
 system_config = {
     "id": "test_device",
     "addresses": [
-        "libcamera_in"
-        "s3_upload",
-    ],
+        "libcamera_in",
+        "s3_upload"
+    ]
 }
-
 configuration_tester.run_test_configuration(system_config)
