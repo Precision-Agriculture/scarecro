10e0beae03b3b5d5708076ed0a1ef6fe938736d7
marzeverett <marzeverett@gmail.com>
2025-03-14 15:50:59 -0700

    Added full camera tasks test

---
+++ tests/test_full_camera_tasks.py
@@ -0,0 +1,12 @@
+import configuration_tester
+system_config = {
+    "id": "test_device",
+    "addresses": [
+        "libcamera_in",
+        "s3_upload"
+    ],
+    "tasks":[
+        "clean_camera_pictures"
+    ]
+}
+configuration_tester.run_test_configuration(system_config)
