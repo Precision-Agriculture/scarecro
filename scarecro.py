f10f3776b2ec28714f6f76a6094aa6736a296adc
marzeverett <marzeverett@gmail.com>
2025-07-10 14:58:27 -0700

    Make some updates

--- scarecro.py
+++ scarecro.py
@@ -5,7 +5,7 @@ import logging
 import system_object
 import src.system.system as system_class 
 #It's possible that logging is something you'll want to set on a system level
-#This should do for now 
+#This should do for now - marked for change 
 logging.basicConfig(level=logging.INFO)
 system_object.system = system_class.return_object()
 system_object.system.start_scheduler()
